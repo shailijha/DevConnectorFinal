80486a2acf6fd47b75058eb9790c190c0d3cfd32
shailijha <shaili_jha@yahoo.co.in>
2019-08-22 22:47:12 -0400

    Added pipe after posts

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,9 +14,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/posts'>Posts</Link>
       </li>
       <li>
+        |
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
+          <span className='hide-sm'> Dashboard</span>
         </Link>
       </li>
       <li>
