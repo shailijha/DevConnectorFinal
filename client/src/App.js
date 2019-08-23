fa2c013da3a35b23ed8fb0fd0dac27d78d0a637c
shailijha <shaili_jha@yahoo.co.in>
2019-08-22 21:29:04 -0400

    Added Private route for Posts

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -64,6 +65,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
