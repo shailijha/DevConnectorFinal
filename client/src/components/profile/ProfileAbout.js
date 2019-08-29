3ee181cff81df41a0c0788df95133ad4381bb898
shailijha <shaili_jha@yahoo.co.in>
2019-08-29 12:28:40 -0400

    Added icons for cs languages in bio

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -7,25 +7,43 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => (
-  <div className='profile-about bg-light p-2'>
-    {bio && (
-      <Fragment>
-        <h2 className='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
-        <p>{bio}</p>
-        <div className='line' />
-      </Fragment>
-    )}
-    <h2 className='text-primary'>Skill Set</h2>
-    <div className='skills'>
-      {skills.map((skill, index) => (
-        <div key={index} className='p-1'>
-          <i className='fas fa-check' /> {skill}
-        </div>
-      ))}
+}) => {
+  const mapLanguageIcons = new Map([
+    ['html', <i class='fab fa-html5 fa-2x' />],
+    ['php', <i class='fab fa-php fa-2x' />],
+    ['java', <i class='fab fa-java fa-2x' />],
+    ['javascript', <i class='fab fa-js-square fa-2x' />],
+    ['js', <i class='fab fa-js-square fa-2x' />],
+    ['react', <i class='fab fa-react fa-2x' />],
+    ['css', <i class='fab fa-css3-alt fa-2x' />],
+    ['python', <i class='fab fa-python fa-2x' />]
+  ]);
+
+  return (
+    <div className='profile-about bg-light p-2'>
+      {bio && (
+        <Fragment>
+          <h2 className='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
+          <p>{bio}</p>
+          <div className='line' />
+        </Fragment>
+      )}
+      <h2 className='text-primary'>Skill Set</h2>
+      <div className='skills'>
+        {skills.map((skill, index) => (
+          <div key={index} className='p-1'>
+            {mapLanguageIcons.has(skill.toLowerCase()) ? (
+              mapLanguageIcons.get(skill.toLowerCase())
+            ) : (
+              <i class='fa fa-check' />
+            )}{' '}
+            {skill}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired
